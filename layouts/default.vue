befdb01c64f6b47b6d0f88d9ac67d757e33a33f0
username40 <52605355+username40@users.noreply.github.com>
2020-02-22 17:04:03 -0300

    create a defualt layout )

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <el-container>
+    <el-header>
+      <app-navigation/>
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+    <el-footer/>
+  </el-container>
 </template>
+
+<script>
+  import AppNavigation from '@/components/main/Navigation'
+
+  export default {
+    components: {
+      AppNavigation
+    }
+  }
+</script>
 
 <style>
 
