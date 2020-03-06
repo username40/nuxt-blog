790c4e7bebd8b667f1fa4e2cbe6d02d120b79323
username40 <52605355+username40@users.noreply.github.com>
2020-03-06 09:56:33 -0300

    create 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="error-layout">
+      <div class="error-content">
+      <h1 class="error-title">Ошибка 404</h1>
+      <nuxt-link to="/">На главную</nuxt-link>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "error"
+    }
+</script>
+
+<style scoped>
+  .error-layout {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .error-content {
+    padding-top: 4rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .error-title {
+    color: rgb(145,6,6);
+    margin-bottom: 1rem;
+  }
+</style>
