930e9132c0ce9d08c3bfa6c287ac889edf943bdf
username40 <52605355+username40@users.noreply.github.com>
2020-03-08 18:36:17 -0300

    start make a admin panel

---
+++ layouts/admin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="admin-layout-wrap">
+  <el-container :style="{height: '100%'}">
+    <el-aside width="200px">
+      <app-aside/>
+    </el-aside>
+    <el-main>
+      <nuxt />
+    </el-main>
+  </el-container>
+  </div>
+</template>
+
+<script>
+  import AppAside from '@/components/admin/Aside'
+    export default {
+        name: "admin"
+    }
+</script>
+
+<style scoped>
+  .admin-layout-wrap {
+    width: 100%;
+    height: 100vh;
+  }
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Welcome to admin page</h1>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        layout: 'admin'
+    }
+</script>
+
+<style scoped>
+
+</style>
