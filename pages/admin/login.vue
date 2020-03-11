06fff1974528dc10e292148ced0f24dcbc37d650
username40 <52605355+username40@users.noreply.github.com>
2020-03-11 18:14:34 -0300

    make some html from login page

---
+++ pages/admin/login.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-card
+    shadow="always"
+    :style="{width: '500px'}">
+    <h2>Войти в панель администратора</h2>
+    <el-form
+      @submit.native.prevent="onSubmit"
+      :model="controls"
+      :rules="rules"
+      ref="form">
+      <el-form-item label="Ваше имя" prop="login">
+        <el-input v-model.trim="controls.login"/>
+      </el-form-item>
+      <div class="mb2">
+      <el-form-item label="Пароль" prop="password">
+        <el-input type="password" v-model="controls.password"/>
+      </el-form-item>
+      </div>
+      <el-form-item>
+        <el-button
+          type="primary"
+          native-type="submit"
+          :loading="loading"
+          round>Войти
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+  export default {
+    layout: 'empty',
+    data() {
+      return {
+        loading: false,
+        controls: {
+          login: '',
+          password: ''
+        },
+        rules: {
+          login: [
+            {required: true, message: 'Введите логин', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: 'Введите пароль', trigger: 'blur'},
+            {min: 6, message: 'Пароль должен быть не менее 6 символов', trigger: 'blur'}
+          ]
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
