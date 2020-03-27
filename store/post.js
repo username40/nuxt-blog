60f1415e53f408d0b23874b060d3a36632d987e6
username40 <52605355+username40@users.noreply.github.com>
2020-03-27 12:42:04 -0300

    start writing work with posts

---
+++ store/post.js
@@ -0,0 +1,12 @@
+export const actions = {
+  async fetchAdmin({}) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          {title: 'Post 1', date: new Date(), views: '20', comments: [1, 2], _id: Math.random()},
+          {title: 'Post 2', date: new Date(), views: '22', comments: [1], _id: Math.random()}
+        ])
+      },2000)
+    })
+  }
+}
