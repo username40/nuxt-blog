af633f9e4a8becec176aa905113ff35cb4ff5fc7
username40 <52605355+username40@users.noreply.github.com>
2020-03-27 15:40:11 -0300

    write about edititng post page UI and logic

--- store/post.js
+++ store/post.js
@@ -1,15 +1,27 @@
+const posts = [
+  {title: 'Post 1', date: new Date(), views: '20', comments: [1, 2], _id: '114149'},
+  {title: 'Post 2', date: new Date(), views: '22', comments: [1], _id: '119158'}
+]
+
 export const actions = {
   async fetchAdmin({}) {
     return await new Promise(resolve => {
       setTimeout(() => {
-        resolve([
-          {title: 'Post 1', date: new Date(), views: '20', comments: [1, 2], _id: Math.random()},
-          {title: 'Post 2', date: new Date(), views: '22', comments: [1], _id: Math.random()}
-        ])
+        resolve(posts)
       },400)
     })
   },
   async remove({}, id) {
 
+  },
+  async update({}, {id, text}) {
+
+  },
+  async fetchAdminById({}, id) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve(posts.find(p => p._id === id))
+      },1000)
+    })
   }
 }
