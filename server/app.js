2d4b0c9a32392e9265546f1d5886ea0fb5a6987b
username40 <52605355+username40@users.noreply.github.com>
2020-04-06 17:52:31 +0200

    connect mongoDB use mongoDB Atlas, new technology  in my money-box

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const keys = require('./keys')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect(keys.MONGO_URI)
+  .then(() => {console.log('mongo db connected!')})
+  .catch(error => console.error(error))
 
 module.exports = app
 
--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-
+  MONGO_URI: `mongodb+srv://tentacula:1A9dnSGmjVxbZaZ0@cluster0-80dbi.gcp.mongodb.net/test?retryWrites=true&w=majority`
 }
