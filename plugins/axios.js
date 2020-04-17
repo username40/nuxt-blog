0d8f7d6a1618e356430215bbf951b502ec5a60f6
username40 <52605355+username40@users.noreply.github.com>
2020-04-17 22:11:08 +0300

    add token to app

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,11 +1,21 @@
 export default function ({ $axios, redirect, store }) {
+
+  $axios.interceptors.request.use(request => {
+    if (store.getters['auth/isAuthenticated'] && !request.headers.common['Authorization']) {
+      const token = store.getters['auth/token']
+      request.headers.common['Authorization'] = `Bearer ${token}`
+    }
+    return request
+  })
+
   $axios.onError(error => {
     if (error.response) {
       if (error.response.status === 401) {
         redirect('/admin/login?message=session')
         store.dispatch('auth/logout')
       }
-      if(error.response.status === 500) {
+
+      if (error.response.status === 500) {
         console.error('Server 500 error')
       }
     }
